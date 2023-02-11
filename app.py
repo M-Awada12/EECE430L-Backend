8f59786d7b32f7e30dddba4a085d2a38df17e2bb
M-Awada <m.awada1212@gmail.com>
2023-02-11 15:46:10 +0200

    backend

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:kali@localhost:3306/exchange'
 db = SQLAlchemy(app)
 
+
 class Transaction(db.Model):
     __tablename__ = 'Transaction'
-    id = db.Column(db.Integer, primary_key=True,unique=True)
-    usd_amount = db.Column(db.Float)
-    lbp_amount = db.Column(db.Float)
-    usd_to_lbp = db.Column(db.Boolean)
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    usd_amount = db.Column(db.Float, nullable=False)
+    lbp_amount = db.Column(db.Float, nullable=False)
+    usd_to_lbp = db.Column(db.Boolean, nullable=False)
 
-    def _init_(self, usd_amount, lbp_amount, usd_to_lbp):
+    def __init__(self, usd_amount, lbp_amount, usd_to_lbp):
         self.usd_amount = usd_amount
         self.lbp_amount = lbp_amount
         self.usd_to_lbp = usd_to_lbp
+
 
 def serialize_transaction(transaction):
     return {
@@ -26,17 +27,18 @@ def serialize_transaction(transaction):
         'usd_to_lbp': transaction.usd_to_lbp
     }
 
+
 @app.route('/transaction', methods=['POST'])
-def createtransaction():
+def create_transaction():
     body = request.get_json()
-    transaction = Transaction(body['usd_amount'], body['lbp_amount'], body['usd_to_lbp'])
-    db.session.add(transaction)
+    new_transaction = Transaction(body['usd_amount'], body['lbp_amount'], body['usd_to_lbp'])
+    db.session.add(new_transaction)
     db.session.commit()
-    return jsonify(serialize_transaction(transaction))
+    return jsonify(serialize_transaction(new_transaction))
 
 
 @app.route("/exchangeRate", methods=['GET'])
-def ExchangeRate():
+def exchange_rate():
     filtered_transactions = db.session.query(Transaction).filter(Transaction.usd_to_lbp == 0)
     average_lbp_amount = sum(t.lbp_amount for t in filtered_transactions) / filtered_transactions.count()
     filtered_transactions2 = db.session.query(Transaction).filter(Transaction.usd_to_lbp == 1)
