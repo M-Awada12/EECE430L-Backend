01fdd3eadb3e98d4ad664e205c21812b6bb95461
M-Awada <m.awada1212@gmail.com>
2023-03-09 13:02:41 +0200

    phase 3 done

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:kali@localhost:3306/exchange'
 CORS(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 class Transaction(db.Model):
     __tablename__ = 'Transaction'
     id = db.Column(db.Integer, primary_key=True, unique=True)
@@ -20,12 +22,40 @@ class Transaction(db.Model):
         self.lbp_amount = lbp_amount
         self.usd_to_lbp = usd_to_lbp
 
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(30), unique=True)
+    hashed_password = db.Column(db.String(128))
+
+    def __init__(self, user_name, password):
+        super(User, self).__init__(user_name=user_name)
+        self.hashed_password = bcrypt.generate_password_hash(password)
+
+
 class TransactionSchema(ma.Schema):
     class Meta:
         fields = ("id", "usd_amount", "lbp_amount", "usd_to_lbp")
         model = Transaction
 
 transaction_schema = TransactionSchema()
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "user_name")
+        model = User
+
+user_schema = UserSchema()
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    new_user = User(body['user_name'], body['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(user_schema.dump(new_user))
+
 
 
 
